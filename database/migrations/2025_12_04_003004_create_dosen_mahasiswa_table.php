dc7613dc4e2e99f6983cb3ddfb6d09e9707f2861
raihan <nayaian973@gmail.com>
2026-01-12 23:02:55 +0800

    ganti migrasi dosen mahasiswa

--- database/migrations/2025_12_04_003004_create_dosen_mahasiswa_table.php
+++ database/migrations/2025_12_04_003004_create_dosen_mahasiswa_table.php
@@ -10,16 +10,24 @@ return new class extends Migration
     {
         Schema::create('dosen_mahasiswa', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('dosen_id')->constrained('users');
-            $table->foreignId('mahasiswa_id')->constrained('users');
+
+            // Jika User Dosen dihapus, data pembimbing ikut terhapus
+            $table->foreignId('dosen_id')->constrained('users')->onDelete('cascade');
+
+            // Jika User Mahasiswa dihapus, data bimbingan ikut terhapus
+            $table->foreignId('mahasiswa_id')->constrained('users')->onDelete('cascade');
+
+            // Relasi ke SK
             $table->foreignId('sk_id')->constrained('surat_keputusan')->onDelete('cascade');
+
             $table->enum('posisi', ['Pembimbing 1', 'Pembimbing 2']);
             $table->timestamps();
 
             // Unique constraint:
             // 1. Satu mahasiswa tidak boleh punya 2 pembimbing 1 di SK yang sama
-            // 2. Satu dosen tidak boleh jadi pembimbing 1 dan 2 untuk mahasiswa yang sama di SK yang sama
             $table->unique(['mahasiswa_id', 'sk_id', 'posisi']);
+
+            // 2. Satu dosen tidak boleh jadi pembimbing 1 dan 2 untuk mahasiswa yang sama di SK yang sama
             $table->unique(['dosen_id', 'mahasiswa_id', 'sk_id']);
         });
     }
